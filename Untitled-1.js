3be0d67722d88c2ff52c9a2f0b08a09c4ad022b5
mohit kumar <107939086+mohitkumar357@users.noreply.github.com>
2024-12-22 21:13:53 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,9 @@
+// Example of smooth scrolling when clicking navigation links (Optional)
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
